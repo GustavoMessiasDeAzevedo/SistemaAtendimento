862ad8c1a0a8c9981da12e663b2c446c7b7193c0
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-08-26 22:06:54 -0300

    Criando arquivo de conexão com o banco de dados

---
+++ Database/ConexaoDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace SistemaAtendimento.Database
+{
+    public static class ConexaoDB
+    {
+        public static SqlConnection GetConexao()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["SistemaAtendimentoDB"].ConnectionString;
+
+            return new SqlConnection(connectionString);
+
+
+        }
+    }
+}

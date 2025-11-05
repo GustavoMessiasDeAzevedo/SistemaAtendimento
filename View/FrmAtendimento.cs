1fdc2807d719d673976529590af85f1e4ef57a9f
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-11-04 21:10:07 -0300

    Habilitar campos e validar dados

--- View/FrmAtendimento.cs
+++ View/FrmAtendimento.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using SistemaAtendimento.Controller;
+﻿using SistemaAtendimento.Model;
 
 namespace SistemaAtendimento.View
 {
@@ -19,7 +10,7 @@ namespace SistemaAtendimento.View
         {
             InitializeComponent();
         }
-          
+
 
         private void btnPesquisarAtendimento_Click(object sender, EventArgs e)
         {
@@ -27,7 +18,68 @@ namespace SistemaAtendimento.View
             frmConsultaAtendimento.ShowDialog();
         }
 
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            HabilitarCampos();
+        }
 
-     
+        private void HabilitarCampos()
+        {
+            cbxNomeCliente.Enabled = true;
+            dtpAberturaAtendimento.Enabled = true;
+            cbxSituacaoAtendimento.Enabled = true;
+            txtObservacaoAtendimento.ReadOnly = false;
+
+            btnNovo.Enabled = false;
+            btnSalvar.Enabled = true;
+            btnCancelar.Enabled = true;
+        }
+
+        private void DesabilitarCampos()
+        {
+            cbxNomeCliente.Enabled = false;
+            dtpAberturaAtendimento.Enabled = false;
+            cbxSituacaoAtendimento.Enabled = false;
+            txtObservacaoAtendimento.ReadOnly = true;
+
+            btnNovo.Enabled = true;
+            btnSalvar.Enabled = false;
+            btnCancelar.Enabled = false;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+            DesabilitarCampos();
+        }
+
+        private void LimparCampos()
+        {
+            cbxEtapaAtendimento.SelectedIndex = -1;
+            txtObservacaoAtendimento.Clear();
+            dtpAberturaAtendimento.Value = DateTime.Now;
+            cbxSituacaoAtendimento.SelectedIndex = -1;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Atendimentos atendimento = new Atendimentos
+            {
+                ClienteId = Convert.ToInt32(txtCodigoCliente.Text),
+                UsuarioId = 1,
+                SituacaoAtendimentoId = Convert.ToInt32(cbxSituacaoAtendimento.SelectedValue),
+                DataAbertura = dtpAberturaAtendimento.Value,
+                Observacao = txtObservacaoAtendimento.Text
+            };
+            if (!ValidarDados(atendimento))
+                return;
+        }
+
+        private bool ValidarDados(Atendimentos atendimento)
+        {
+            //Criar Regras de validação de campos
+
+            return true;
+        }
     }
 }
